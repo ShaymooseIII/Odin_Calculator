c9034da247f7bafbd4f091f52b7077c432bf1ab3
Seamus Xubuntu <151066590+ShaymooseIII@users.noreply.github.com>
2024-10-03 14:49:39 -0700

    added functionality to the button presses
    the button presses now show up on the display.
    Also made sure only button presses are registered and
    not the empty divs.

--- javascript.js
+++ javascript.js
@@ -19,6 +19,7 @@ function divide(num1,num2){
 let firstNum = 0;
 let secondNum = 0;
 let operator = '';
+const operators = ['+', "-", "/", "x", "="]
 //operate Function
 function operate(operator, num1, num2){
     switch(operator){
@@ -33,5 +34,32 @@ function operate(operator, num1, num2){
     }
 
 }
+//update display function 
+function updateDisplay(num){
+    display.textContent += num;
+}
+//document queries 
+const body = document.querySelector(".body");
+const display = document.querySelector(".display");
 
-console.log(operate('+',1,3))
+//event listener for the calculator body
+body.addEventListener("click",(e)=>{
+    if(e.target.tagName === 'BUTTON'){
+        //take in the button inputs into variables
+        option = e.target.textContent;
+        console.log(option);
+    }
+    if(operators.includes(option)){
+        console.log("in operator");
+        return;
+    }else if(option === 'clear'){
+        console.log("will clear");
+        return;
+    }else{
+        console.log("number");
+        updateDisplay(option);
+    }
+})
+    //display the inputs
+    //pass the inputs into operate 
+    //display operate output 
